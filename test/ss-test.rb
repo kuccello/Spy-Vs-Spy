c88b330519abd71fee9ad9f880c429441dcfb6ad
Joshua Hull <joshbuddy@gmail.com>
2009-11-23 18:55:41 -0500

    committed new gemspec

--- test/ss-test.rb
+++ test/ss-test.rb
@@ -23,29 +23,17 @@ class SpyVsSpyTest < Test::Unit::TestCase
     contains.each do |filename|
 
       file = File.new("#{File.dirname(__FILE__)}/safari/#{filename}", "r")
-
-#      puts <<-r
-#filename: #{filename}
-#      r
       rx =  Regexp.new("^safari(\-(([0-9])(.*)))\.txt")
       rx.match(filename)
-      #puts "#{__FILE__}:#{__LINE__} #{__method__} #{$1} - #{$2} - #{$3} - #{$4} - #{$5} - #{$6} - #{$7}"
       major_version = $3
-#      puts "#{__FILE__}:#{__LINE__} #{__method__} #{major_version}"
 
       while (line = file.gets)
-
-#        puts "#{__FILE__}:#{__LINE__} #{__method__} #{line}"
         r = SoldierOfCode::SpyVsSpy.new(line)
-#        puts "#{__FILE__}:#{__LINE__} #{__method__} BROWSER: #{r.browser}"
         assert r.browser == 'Safari'
 
         if major_version && major_version.length > 0
-#          puts "#{__FILE__}:#{__LINE__} #{__method__} #{line}"
-#          puts "#{__FILE__}:#{__LINE__} #{__method__} #{r.browser_version_major} == #{major_version} ? #{r.browser_version_major == major_version}"
           assert r.browser_version_major == major_version
         end
-
         ua_count+=1
 
       end
@@ -70,39 +58,22 @@ class SpyVsSpyTest < Test::Unit::TestCase
     contains.each do |filename|
 
       file = File.new("#{File.dirname(__FILE__)}/firefox/#{filename}", "r")
-
-#      puts <<-r
-#filename: #{filename}
-#      r
       rx =  Regexp.new("^firefox(\-(([0-9])(.*)))\.txt")
       rx.match(filename)
-      #puts "#{__FILE__}:#{__LINE__} #{__method__} #{$1} - #{$2} - #{$3} - #{$4} - #{$5} - #{$6} - #{$7}"
       major_version = $3
-#      puts "#{__FILE__}:#{__LINE__} #{__method__} #{major_version}"
-
       while (line = file.gets)
-
-#        puts "#{__FILE__}:#{__LINE__} #{__method__} #{line}"
         r = SoldierOfCode::SpyVsSpy.new(line)
-#        puts "#{__FILE__}:#{__LINE__} #{__method__} BROWSER: #{r.browser}"
         assert r.browser == 'Firefox'
 
         if major_version && major_version.length > 0
-#          puts "#{__FILE__}:#{__LINE__} #{__method__} #{line}"
-#          puts "#{__FILE__}:#{__LINE__} #{__method__} [#{r.browser_version_major}] == [#{major_version}] ? #{r.browser_version_major == major_version}"
           assert r.browser_version_major == major_version
         end
 
         ua_count+=1
-
       end
-
-
     end
 
     puts "Number of firefox agents tested: #{ua_count}"
-
-
   end
 
   def test_msie_parse
@@ -116,40 +87,23 @@ class SpyVsSpyTest < Test::Unit::TestCase
 
     contains.each do |filename|
 
-      file = File.new("#{File.dirname(__FILE__)}/msie/#{filename}", "r")
-#
-#      puts <<-r
-#filename: #{filename}
-#      r
+      file = File.new("#{File.dirname(__FILE__)}/msie/#{filename.sub("\n","")}", "r")
       rx =  Regexp.new("^msie(\-(([0-9])(.*)))\.txt")
       rx.match(filename)
-      #puts "#{__FILE__}:#{__LINE__} #{__method__} #{$1} - #{$2} - #{$3} - #{$4} - #{$5} - #{$6} - #{$7}"
       major_version = $3
-#      puts "#{__FILE__}:#{__LINE__} #{__method__} #{major_version}"
 
       while (line = file.gets)
 
-#        puts "#{__FILE__}:#{__LINE__} #{__method__} #{line}"
         r = SoldierOfCode::SpyVsSpy.new(line)
-#        puts "#{__FILE__}:#{__LINE__} #{__method__} BROWSER: #{r.browser}"
         assert r.browser == 'MSIE'
 
         if major_version && major_version.length > 0
-#          puts "#{__FILE__}:#{__LINE__} #{__method__} #{line}"
-#          puts "#{__FILE__}:#{__LINE__} #{__method__} [#{r.browser_version_major}] == [#{major_version}] ? #{r.browser_version_major == major_version}"
           assert r.browser_version_major == major_version
         end
-
         ua_count+=1
-
       end
-
-
     end
 
     puts "Number of internet explorer agents tested: #{ua_count}"
-
-
   end
-
 end
